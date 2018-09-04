3a94682810d3269c12ce36821c1e4345ccbfcc72
Arif Hossain <arif.hossain@thedailystar.net>
2018-09-04 17:24:23 +0600

    new template done for single post

--- wp-content/themes/alpha/single.php
+++ wp-content/themes/alpha/single.php
@@ -15,28 +15,26 @@
         </div>
     </div>
 </div>
+
 <div class="posts">
     <?php while( have_posts() ):
     the_post();
     ?>
     <div class="post" <?php post_class(); ?>>
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                </div>
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <h2 class="post-title text-center"><?php the_title(); ?></h2>
+                <p class="text-center">
+                  <strong><?php the_author(); ?></strong><br/>
+                  <?php echo get_the_date(); ?>
+                </p>
             </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <p>
-                        <strong><?php the_author(); ?></strong><br/>
-                        <?php echo get_the_date(); ?>
-                    </p>
-                    <?php echo get_the_tag_list("<ul class=\"list-unstyled\"><li>","</li><li>","</li></ul>") ?>
+        </div>
 
-                </div>
-                <div class="col-md-8">
-                     <?php if (has_post_thumbnail()): ?>
+            <div class="row">
+                <div class="col-md-10 offset-md-1">
+                    <?php if (has_post_thumbnail()): ?>
                         <p><?php the_post_thumbnail("large", array("class" => 'img-fluid')); ?></p>
                     <?php endif ?>
                     <?php if (is_single()) {
@@ -46,24 +44,9 @@
                     } ?>
                 </div>
             </div>
-
-        </div>
     </div>
 
     <?php endwhile; ?>
-
-    <div class="container post-pagination">
-        <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-8">
-                <?php the_posts_pagination(array(
-                    'screen_reader_text' => ' ',
-                    'prev_text'          => 'New posts',
-                    'next_text'          => 'Old posts'
-                )); ?>
-            </div>
-        </div>
-    </div>
 
 </div>
 <div class="footer">
