d44eb1b3e7195f130471180cbdbc0f0c99088977
Arif Hossain <arif.hossain@thedailystar.net>
2018-09-04 17:01:23 +0600

    post single view done

--- wp-content/themes/alpha/index.php
+++ wp-content/themes/alpha/index.php
@@ -23,7 +23,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="post-title"><?php the_title(); ?></h2>
+                    <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                 </div>
             </div>
             <div class="row">
@@ -39,7 +39,11 @@
                      <?php if (has_post_thumbnail()): ?>
                         <p><?php the_post_thumbnail("large", array("class" => 'img-fluid')); ?></p>
                     <?php endif ?>
-                    <?php the_excerpt(); ?>
+                    <?php if (is_single()) {
+                        the_content();
+                    } else{
+                        the_excerpt();
+                    } ?>
                 </div>
             </div>
 
