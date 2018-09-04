689ffb9497a30ceb293c0268e800704b1c1887a3
Arif Hossain <arif.hossain@thedailystar.net>
2018-09-04 17:33:12 +0600

    comment form in single post view done

--- wp-content/themes/alpha/single.php
+++ wp-content/themes/alpha/single.php
@@ -43,6 +43,13 @@
                         the_excerpt();
                     } ?>
                 </div>
+                <?php if(comments_open()) : ?>
+
+                    <div class="col-md-10 offset-md-1">
+                        <?php comments_template(); ?>
+                    </div>
+
+                <?php endif; ?>
             </div>
     </div>
 
