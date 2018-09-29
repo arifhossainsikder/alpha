fe99682bf5bfd570c338bec49d43bedc36c5bebb
Arif <arifcse11@gmail.com>
2018-09-29 22:30:36 +0600

    register widget area and show sidebar in single post

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -1,17 +1,35 @@
 <?php
 
-function alpha_bootstraping(){
-  load_theme_textdomain("alpha");
-  add_theme_support("post-thumbnails");
-  add_theme_support("title-tag");
+function alpha_bootstraping()
+{
+	load_theme_textdomain("alpha");
+	add_theme_support("post-thumbnails");
+	add_theme_support("title-tag");
 }
 
-add_action("after_setup_theme","alpha_bootstraping");
+add_action("after_setup_theme", "alpha_bootstraping");
 
 
-function alpha_assets(){
-  wp_enqueue_style("alpha", get_stylesheet_uri());
-  wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+function alpha_assets()
+{
+	wp_enqueue_style("alpha", get_stylesheet_uri());
+	wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
 }
 
-add_action("wp_enqueue_scripts","alpha_assets");
+add_action("wp_enqueue_scripts", "alpha_assets");
+
+function alpha_sidebar()
+{
+   register_sidebar(
+   	   array(
+   		   'name'          => __( 'Single Post Sidebar', 'alpha' ),
+   		   'id'            => 'sidebar-1',
+   		   'description'   => __( 'Right Sidebar', 'alpha' ),
+   		   'before_widget' => '<li id="%1$s" class="widget %2$s">',
+   		   'after_widget'  => '</li>',
+   		   'before_title'  => '<h2 class="widgettitle">',
+   		   'after_title'   => '</h2>' )
+      );
+}
+
+add_action("widgets_init", "alpha_sidebar");
--- wp-content/themes/alpha/single.php
+++ wp-content/themes/alpha/single.php
@@ -1,53 +1,82 @@
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
-<?php get_template_part("hero"); ?>
-
-<div class="posts">
-    <?php while( have_posts() ):
-    the_post();
-    ?>
-    <div class="post" <?php post_class(); ?>>
-        <div class="container">
+<?php get_template_part( "hero" ); ?>
+    <div class="container">
         <div class="row">
-            <div class="col-md-10 offset-md-1">
-                <h2 class="post-title text-center"><?php the_title(); ?></h2>
-                <p class="text-center">
-                  <strong><?php the_author(); ?></strong><br/>
-                  <?php echo get_the_date(); ?>
-                </p>
+            <div class="col-md-8">
+                <div class="posts">
+					<?php
+					while ( have_posts() ) :
+						the_post();
+						?>
+                        <div class="post" <?php post_class(); ?>>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h2 class="post-title">
+											<?php the_title(); ?>
+                                        </h2>
+                                        <p class="">
+                                            <em><?php the_author(); ?></em><br/>
+											<?php echo get_the_date(); ?>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <p>
+											<?php
+											if ( has_post_thumbnail() ) {
+												the_post_thumbnail( "large", array( "class" => "img-fluid" ) );
+											}
+
+											the_content();
+
+											next_post_link();
+											echo "<br/>";
+											previous_post_link();
+
+											?>
+                                        </p>
+                                    </div>
+									<?php /*if ( comments_open() ): */?><!--
+                            <div class="col-md-10 offset-md-1">
+                                <?php
+									/*                                comments_template();
+																	*/?>
+                            </div>
+                        --><?php /*endif; */?>
+                                </div>
+                            </div>
+                        </div>
+					<?php
+					endwhile;
+					?>
+
+                    <div class="container post-pagination">
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-8">
+								<?php
+								the_posts_pagination( array(
+									"screen_reader_text" => ' ',
+									"prev_text"          => "New Posts",
+									"next_text"          => "Old Posts"
+								) );
+								?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+				<?php
+				if(is_active_sidebar("sidebar-1")){
+					dynamic_sidebar("sidebar-1");
+				}
+				?>
             </div>
         </div>
-
-            <div class="row">
-                <div class="col-md-10 offset-md-1">
-                    <?php if (has_post_thumbnail()): ?>
-                        <p><?php the_post_thumbnail("large", array("class" => 'img-fluid')); ?></p>
-                    <?php endif ?>
-                   <?php
-
-                   the_content();
-
-                   next_post_link();
-
-                   echo '<br>';
-
-                   previous_post_link();
-
-
-                    ?>
-                </div>
-                <?php if(comments_open()) : ?>
-
-                    <div class="col-md-10 offset-md-1">
-                        <?php comments_template(); ?>
-                    </div>
-
-                <?php endif; ?>
-            </div>
     </div>
-
-    <?php endwhile; ?>
-
-</div>
 
 <?php get_footer(); ?>
