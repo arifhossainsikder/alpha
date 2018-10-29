7f8fd0416834b8372ae897eb4c6381257b800972
Arif Hossain <arif.hossain@thedailystar.net>
2018-10-29 13:42:43 +0600

    Show widgets in footer

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -1,35 +1,57 @@
 <?php
 
-function alpha_bootstraping()
-{
-	load_theme_textdomain("alpha");
-	add_theme_support("post-thumbnails");
-	add_theme_support("title-tag");
+function alpha_bootstraping() {
+	load_theme_textdomain( "alpha" );
+	add_theme_support( "post-thumbnails" );
+	add_theme_support( "title-tag" );
 }
 
-add_action("after_setup_theme", "alpha_bootstraping");
+add_action( "after_setup_theme", "alpha_bootstraping" );
 
 
-function alpha_assets()
-{
-	wp_enqueue_style("alpha", get_stylesheet_uri());
-	wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+function alpha_assets() {
+	wp_enqueue_style( "alpha", get_stylesheet_uri() );
+	wp_enqueue_style( "bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" );
 }
 
-add_action("wp_enqueue_scripts", "alpha_assets");
+add_action( "wp_enqueue_scripts", "alpha_assets" );
 
-function alpha_sidebar()
-{
-   register_sidebar(
-   	   array(
-   		   'name'          => __( 'Single Post Sidebar', 'alpha' ),
-   		   'id'            => 'sidebar-1',
-   		   'description'   => __( 'Right Sidebar', 'alpha' ),
-   		   'before_widget' => '<li id="%1$s" class="widget %2$s">',
-   		   'after_widget'  => '</li>',
-   		   'before_title'  => '<h2 class="widgettitle">',
-   		   'after_title'   => '</h2>' )
-      );
+function alpha_sidebar() {
+	register_sidebar(
+		array(
+			'name'          => __( 'Single Post Sidebar', 'alpha' ),
+			'id'            => 'sidebar-1',
+			'description'   => __( 'Right Sidebar', 'alpha' ),
+			'before_widget' => '<li id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</li>',
+			'before_title'  => '<h2 class="widgettitle">',
+			'after_title'   => '</h2>'
+		)
+	);
+
+	register_sidebar(
+		array(
+			'name'          => __( 'Footer Left', 'alpha' ),
+			'id'            => 'footer-left',
+			'description'   => __( 'Widgetized area for left side', 'alpha' ),
+			'before_widget' => '',
+			'after_widget'  => '',
+			'before_title'  => '',
+			'after_title'   => ''
+		)
+	);
+
+	register_sidebar(
+		array(
+			'name'          => __( 'Footer Right', 'alpha' ),
+			'id'            => 'footer-right',
+			'description'   => __( 'Widgetized area for right side', 'alpha' ),
+			'before_widget' => '',
+			'after_widget'  => '',
+			'before_title'  => '',
+			'after_title'   => ''
+		)
+	);
 }
 
-add_action("widgets_init", "alpha_sidebar");
+add_action( "widgets_init", "alpha_sidebar" );
