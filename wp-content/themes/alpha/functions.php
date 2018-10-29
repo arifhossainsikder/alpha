d8579845bf8e7d7d54c5aa67e6ee185f15460c44
Arif Hossain <arif.hossain@thedailystar.net>
2018-10-29 15:42:04 +0600

    create menu location, show menu, add extra css to menu item

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -4,6 +4,8 @@ function alpha_bootstraping() {
 	load_theme_textdomain( "alpha" );
 	add_theme_support( "post-thumbnails" );
 	add_theme_support( "title-tag" );
+	register_nav_menu("topmenu",__("Top Menu","alpha"));
+	register_nav_menu("footermenu",__("Footer Menu","alpha"));
 }
 
 add_action( "after_setup_theme", "alpha_bootstraping" );
@@ -70,3 +72,10 @@ function alpha_protected_title_change_format(){
 	return "%s";
 }
 add_filter("protected_title_format","alpha_protected_title_change_format");
+
+
+function alpha_menu_item_class($classes,$item){
+	$classes[] = "list-inline-item";
+	return $classes;
+}
+add_filter("nav_menu_css_class","alpha_menu_item_class",10,2);
