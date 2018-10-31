ddf839be4845012595791274a024cb9cbd40647a
Arif <arifcse11@gmail.com>
2018-11-01 00:48:52 +0600

    custom 404 page

---
+++ wp-content/themes/alpha/404.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+<body class="<?php body_class() ?>">
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="text-center">
+				<?php _e("Eroor: Nothing found!","alpha") ?>
+			</h1>
+		</div>
+	</div>
+</div>
+</body>
+<?php get_footer(); ?>
