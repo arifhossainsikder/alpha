fd1b9d6d64ecad532cc405254d4bd3c651952f4b
Arif <arifcse11@gmail.com>
2018-10-31 23:53:23 +0600

    change custom header text color with customizer

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -1,123 +1,146 @@
 <?php
 
-if (site_url()=='http://localhost/lwhh'){
-	define("VERSION",time());
-} else{
-	define("VERSION",wp_get_theme()->get("Version"));
+if (site_url() == 'http://localhost/lwhh') {
+	define("VERSION", time());
+} else {
+	define("VERSION", wp_get_theme()->get("Version"));
 }
 
-function alpha_bootstraping() {
-	load_theme_textdomain( "alpha" );
-	add_theme_support( "post-thumbnails" );
-	add_theme_support( "title-tag" );
-	add_theme_support("custom-header");
-	register_nav_menu( "topmenu", __( "Top Menu", "alpha" ) );
-	register_nav_menu( "footermenu", __( "Footer Menu", "alpha" ) );
+function alpha_bootstraping()
+{
+	load_theme_textdomain("alpha");
+	add_theme_support("post-thumbnails");
+	add_theme_support("title-tag");
+	$alpha_custom_header_setails = array(
+		'header-text' => true,
+		'default-text-color' => '#222',
+
+	);
+	add_theme_support("custom-header", $alpha_custom_header_setails);
+	add_theme_support("header-text");
+	register_nav_menu("topmenu", __("Top Menu", "alpha"));
+	register_nav_menu("footermenu", __("Footer Menu", "alpha"));
 }
 
-add_action( "after_setup_theme", "alpha_bootstraping" );
+add_action("after_setup_theme", "alpha_bootstraping");
 
 
-function alpha_assets() {
-	wp_enqueue_style( "alpha", get_stylesheet_uri(),null,VERSION );
-	wp_enqueue_style( "bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" );
-	wp_enqueue_style( "featherlight-css", "//cdn.rawgit.com/noelboss/featherlight/1.7.13/release/featherlight.min.css" );
+function alpha_assets()
+{
+	wp_enqueue_style("alpha", get_stylesheet_uri(), null, VERSION);
+	wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+	wp_enqueue_style("featherlight-css", "//cdn.rawgit.com/noelboss/featherlight/1.7.13/release/featherlight.min.css");
 
-	wp_enqueue_script( "featherlight-js", "//cdn.rawgit.com/noelboss/featherlight/1.7.13/release/featherlight.min.js", array( "jquery" ), VERSION, true );
-	wp_enqueue_script("alpha-main",get_theme_file_uri("/assets/js/main.js"),array("jquery","featherlight-js"), VERSION, true);
+	wp_enqueue_script("featherlight-js", "//cdn.rawgit.com/noelboss/featherlight/1.7.13/release/featherlight.min.js", array("jquery"), VERSION, true);
+	wp_enqueue_script("alpha-main", get_theme_file_uri("/assets/js/main.js"), array("jquery", "featherlight-js"), VERSION, true);
 
 }
 
-add_action( "wp_enqueue_scripts", "alpha_assets" );
+add_action("wp_enqueue_scripts", "alpha_assets");
 
-function alpha_sidebar() {
+function alpha_sidebar()
+{
 	register_sidebar(
 		array(
-			'name'          => __( 'Single Post Sidebar', 'alpha' ),
-			'id'            => 'sidebar-1',
-			'description'   => __( 'Right Sidebar', 'alpha' ),
+			'name' => __('Single Post Sidebar', 'alpha'),
+			'id' => 'sidebar-1',
+			'description' => __('Right Sidebar', 'alpha'),
 			'before_widget' => '<li id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</li>',
-			'before_title'  => '<h2 class="widgettitle">',
-			'after_title'   => '</h2>'
+			'after_widget' => '</li>',
+			'before_title' => '<h2 class="widgettitle">',
+			'after_title' => '</h2>'
 		)
 	);
 
 	register_sidebar(
 		array(
-			'name'          => __( 'Footer Left', 'alpha' ),
-			'id'            => 'footer-left',
-			'description'   => __( 'Widgetized area for left side', 'alpha' ),
+			'name' => __('Footer Left', 'alpha'),
+			'id' => 'footer-left',
+			'description' => __('Widgetized area for left side', 'alpha'),
 			'before_widget' => '',
-			'after_widget'  => '',
-			'before_title'  => '',
-			'after_title'   => ''
+			'after_widget' => '',
+			'before_title' => '',
+			'after_title' => ''
 		)
 	);
 
 	register_sidebar(
 		array(
-			'name'          => __( 'Footer Right', 'alpha' ),
-			'id'            => 'footer-right',
-			'description'   => __( 'Widgetized area for right side', 'alpha' ),
+			'name' => __('Footer Right', 'alpha'),
+			'id' => 'footer-right',
+			'description' => __('Widgetized area for right side', 'alpha'),
 			'before_widget' => '',
-			'after_widget'  => '',
-			'before_title'  => '',
-			'after_title'   => ''
+			'after_widget' => '',
+			'before_title' => '',
+			'after_title' => ''
 		)
 	);
 }
 
-add_action( "widgets_init", "alpha_sidebar" );
+add_action("widgets_init", "alpha_sidebar");
 
-function alpha_the_excerpt( $excerpt ) {
-	if ( ! post_password_required() ) {
+function alpha_the_excerpt($excerpt)
+{
+	if (!post_password_required()) {
 		return $excerpt;
 	} else {
 		echo get_the_password_form();
 	}
 }
 
-add_filter( "the_excerpt", "alpha_the_excerpt" );
+add_filter("the_excerpt", "alpha_the_excerpt");
 
 
-function alpha_protected_title_change_format() {
+function alpha_protected_title_change_format()
+{
 	return "%s";
 }
 
-add_filter( "protected_title_format", "alpha_protected_title_change_format" );
+add_filter("protected_title_format", "alpha_protected_title_change_format");
 
 
-function alpha_menu_item_class( $classes, $item ) {
+function alpha_menu_item_class($classes, $item)
+{
 	$classes[] = "list-inline-item";
 
 	return $classes;
 }
 
-add_filter( "nav_menu_css_class", "alpha_menu_item_class", 10, 2 );
+add_filter("nav_menu_css_class", "alpha_menu_item_class", 10, 2);
 
 
-function alpha_about_page_template_banner(){
-	if(is_page()) {
-		$alpha_feat_image = get_the_post_thumbnail_url(null,"large");
+function alpha_about_page_template_banner()
+{
+	if (is_page()) {
+		$alpha_feat_image = get_the_post_thumbnail_url(null, "large");
 		?>
-		<style>
-			.page-header{
-				background-image: url(<?php echo $alpha_feat_image;?>);
-			}
-		</style>
+        <style>
+            .page-header {
+                background-image: url(<?php echo $alpha_feat_image;?>);
+            }
+        </style>
 		<?php
 	}
-	if (is_front_page()){
-	    ?>
-<style>
-    .header{
-        background-image: url(<?php echo header_image();?>);
-        background-size: cover;
-        margin-bottom: 10px;
-    }
-</style>
-<?php
-    }
+	if (is_front_page()) {
+		?>
+        <style>
+            .header {
+                background-image: url(<?php echo header_image();?>);
+                background-size: cover;
+                margin-bottom: 10px;
+            }
+
+            .header h1.heading, h3.tagline{
+                color: #<?php echo get_header_textcolor();?>;
+                <?php
+                if (!display_header_text()){
+                    echo "display: none";
+                }
+                ?>
+            }
+        </style>
+		<?php
+	}
 }
-add_action("wp_head","alpha_about_page_template_banner",11);
+
+add_action("wp_head", "alpha_about_page_template_banner", 11);
