d4a6c2dd0869a8ce2dea2c5e843eda5b35438938
Arif <arifcse11@gmail.com>
2018-11-05 21:45:42 +0600

    testimonials with attachments plugin

--- wp-content/themes/alpha/lib/attachments.php
+++ wp-content/themes/alpha/lib/attachments.php
@@ -42,3 +42,58 @@ function alpha_attachments( $attachments )
 }
 
 add_action( 'attachments_register', 'alpha_attachments' );
+
+
+function alpha_testimonial_attachments( $attachments )
+{
+	$fields         = array(
+		array(
+			'name'      => 'name',                         // unique field name
+			'type'      => 'text',                          // registered field type
+			'label'     => __( 'Name', 'attachments' ),    // label to display
+		),
+		array(
+			'name'      => 'position',                         // unique field name
+			'type'      => 'text',                          // registered field type
+			'label'     => __( 'Position', 'attachments' ),    // label to display
+		),
+		array(
+			'name'      => 'company',                         // unique field name
+			'type'      => 'text',                          // registered field type
+			'label'     => __( 'Company', 'attachments' ),    // label to display
+		),
+		array(
+			'name'      => 'testimonial',                         // unique field name
+			'type'      => 'textarea',                          // registered field type
+			'label'     => __( 'Testimonial', 'attachments' ),    // label to display
+		)
+	);
+
+	$args = array(
+
+		// title of the meta box (string)
+		'label'         => 'Testimonial section',
+
+		// all post types to utilize (string|array)
+		'post_type'     => array( 'page'),
+
+
+		// allowed file type(s) (array) (image|video|text|audio|application)
+		'filetype'      => array('image'),  // no filetype limit
+
+		// include a note within the meta box (string)
+		'note'          => 'Add testimonials!',
+
+
+		// text for 'Attach' button in meta box (string)
+		'button_text'   => __( 'Add testimonial', 'alpha' ),
+
+		// fields array
+		'fields'        => $fields,
+
+	);
+
+	$attachments->register( 'testimonials', $args ); // unique instance name
+}
+
+add_action( 'attachments_register', 'alpha_testimonial_attachments' );
--- wp-content/themes/alpha/page-templates/about-page-template.php
+++ wp-content/themes/alpha/page-templates/about-page-template.php
@@ -2,34 +2,64 @@
 get_header();
 ?>
 <body <?php body_class(); ?>>
-<?php get_template_part( "/template-parts/about-page/hero-page" ); ?>
+<?php get_template_part("/template-parts/about-page/hero-page"); ?>
 <div class="posts">
 	<?php
-	while ( have_posts() ) :
+	while (have_posts()) :
 		the_post();
 		?>
-		<div class="post" <?php post_class(); ?>>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-10 offset-md-1">
-						<h2 class="post-title text-center">
+        <div class="post" <?php post_class(); ?>>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 offset-md-1">
+                        <h2 class="post-title text-center">
 							<?php the_title(); ?>
-						</h2>
-						<p class="text-center">
-							<em><?php the_author(); ?></em><br/>
+                        </h2>
+                        <p class="text-center">
+                            <em><?php the_author(); ?></em><br/>
 							<?php echo get_the_date(); ?>
-						</p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-10 offset-md-1">
-						<p>
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row">
+                        <div class="col-md-8 offset-md-2">
+                            <?php
+								if (class_exists('Attachments')) {
+								    ?>
+                                    <h2 class="text-center"><?php _e("Testimonials") ?></h2>
+
+								<?php	}  ?>
+                            <div class="testimonials slider text-center">
+								<?php
+								if (class_exists('Attachments')) {
+									$attachments = new Attachments('testimonials');
+									if ($attachments->exist()) {
+										while ($attachment = $attachments->get()) {
+											?>
+                                            <div>
+												<?php echo $attachments->image("thumbnail"); ?>
+                                                <h4><?php echo esc_html($attachments->field("name")); ?></h4>
+                                                <p><?php echo esc_html($attachments->field("testimonial")); ?></p>
+                                                <p><?php echo esc_html($attachments->field("position")); ?>
+                                                    , <?php echo esc_html($attachments->field("company")); ?></p>
+                                            </div>
+
+										<?php }
+									}
+								}
+								?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-10 offset-md-1">
+                        <p>
 							<?php
-							if ( has_post_thumbnail() ) {
+							if (has_post_thumbnail()) {
 //											    $thumbnail_url = get_the_post_thumbnail_url(null,"large");
 //											    printf("<a href='#' data-featherlight='%s'>",$thumbnail_url);
 								echo "<a class='popup' href='#' data-featherlight='#'>";
-								the_post_thumbnail( "large", array( "class" => "img-fluid" ) );
+								the_post_thumbnail("large", array("class" => "img-fluid"));
 								echo '</a>';
 							}
 
@@ -40,30 +70,30 @@ get_header();
 							previous_post_link();
 
 							?>
-						</p>
-					</div>
+                        </p>
+                    </div>
 
-				</div>
-			</div>
-		</div>
+                </div>
+            </div>
+        </div>
 	<?php
 	endwhile;
 	?>
 
-	<div class="container post-pagination">
-		<div class="row">
-			<div class="col-md-4"></div>
-			<div class="col-md-8">
+    <div class="container post-pagination">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-8">
 				<?php
-				the_posts_pagination( array(
+				the_posts_pagination(array(
 					"screen_reader_text" => ' ',
-					"prev_text"          => "New Posts",
-					"next_text"          => "Old Posts"
-				) );
+					"prev_text" => "New Posts",
+					"next_text" => "Old Posts"
+				));
 				?>
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
