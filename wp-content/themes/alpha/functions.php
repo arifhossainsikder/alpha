e8eb787b47aca32aed9a05149f62f4580d50e24e
Arif <arifcse11@gmail.com>
2018-12-19 23:45:58 +0600

    cmb2 service and pricing table

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -49,6 +49,7 @@ function alpha_assets()
 	wp_enqueue_style("featherlight-css", "//cdn.rawgit.com/noelboss/featherlight/1.7.13/release/featherlight.min.css");
 	wp_enqueue_style("dashicons");
 	wp_enqueue_style("tiny-css", "//cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.8.7/tiny-slider.css");
+	wp_enqueue_style("fontawesome","//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css");
 	wp_enqueue_style("alpha", get_stylesheet_uri(), null, VERSION);
 
 	wp_enqueue_script("featherlight-js", "//cdn.rawgit.com/noelboss/featherlight/1.7.13/release/featherlight.min.js", array("jquery"), VERSION, true);
