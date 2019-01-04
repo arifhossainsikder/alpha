542aa8efd53f3ba5be3e478c9be9fbde86483ba8
Arif <arifcse11@gmail.com>
2019-01-04 15:54:31 +0600

    header navigation done

--- wp-content/themes/philosophy/functions.php
+++ wp-content/themes/philosophy/functions.php
@@ -14,24 +14,26 @@ function philosophy_theme_setup()
 	add_theme_support('html5', array('search-form', 'comment-list'));
 	add_theme_support("post-formats", array("image", "video", "gallery", "quote", "link", "audio"));
 	add_editor_style("/assets/css/editor-style.css");
+	register_nav_menu("topmenu", __("Top Menu", "philosophy"));
 }
 
 add_action("after_setup_theme", "philosophy_theme_setup");
 
-function philosophy_assets() {
-	wp_enqueue_style( "fontawesome-css", get_theme_file_uri( "/assets/css/font-awesome/css/font-awesome.css" ), null, "1.0" );
-	wp_enqueue_style( "fonts-css", get_theme_file_uri( "/assets/css/fonts.css" ), null, "1.0" );
-	wp_enqueue_style( "base-css", get_theme_file_uri( "/assets/css/base.css" ), null, "1.0" );
-	wp_enqueue_style( "vendor-css", get_theme_file_uri( "/assets/css/vendor.css" ), null, "1.0" );
-	wp_enqueue_style( "main-css", get_theme_file_uri( "/assets/css/main.css" ), null, "1.0" );
-	wp_enqueue_style( "philosophy-css", get_stylesheet_uri(), null, VERSION );
+function philosophy_assets()
+{
+	wp_enqueue_style("fontawesome-css", get_theme_file_uri("/assets/css/font-awesome/css/font-awesome.css"), null, "1.0");
+	wp_enqueue_style("fonts-css", get_theme_file_uri("/assets/css/fonts.css"), null, "1.0");
+	wp_enqueue_style("base-css", get_theme_file_uri("/assets/css/base.css"), null, "1.0");
+	wp_enqueue_style("vendor-css", get_theme_file_uri("/assets/css/vendor.css"), null, "1.0");
+	wp_enqueue_style("main-css", get_theme_file_uri("/assets/css/main.css"), null, "1.0");
+	wp_enqueue_style("philosophy-css", get_stylesheet_uri(), null, VERSION);
 
-	wp_enqueue_script( "modernizr-js", get_theme_file_uri( "/assets/js/modernizr.js" ), null, "1.0" );
-	wp_enqueue_script( "pace-js", get_theme_file_uri( "/assets/js/pace.min.js" ), null, "1.0" );
-	wp_enqueue_script( "plugins-js", get_theme_file_uri( "/assets/js/plugins.js" ), array( "jquery" ), "1.0", true );
-	wp_enqueue_script( "main-js", get_theme_file_uri( "/assets/js/main.js" ), array( "jquery" ), "1.0", true );
+	wp_enqueue_script("modernizr-js", get_theme_file_uri("/assets/js/modernizr.js"), null, "1.0");
+	wp_enqueue_script("pace-js", get_theme_file_uri("/assets/js/pace.min.js"), null, "1.0");
+	wp_enqueue_script("plugins-js", get_theme_file_uri("/assets/js/plugins.js"), array("jquery"), "1.0", true);
+	wp_enqueue_script("main-js", get_theme_file_uri("/assets/js/main.js"), array("jquery"), "1.0", true);
 }
 
-add_action( "wp_enqueue_scripts", "philosophy_assets" );
+add_action("wp_enqueue_scripts", "philosophy_assets");
 
 
--- wp-content/themes/philosophy/header.php
+++ wp-content/themes/philosophy/header.php
@@ -2,15 +2,15 @@
 <html class="no-js" lang="en">
 <head>
 
-	<!--- basic page needs
+    <!--- basic page needs
 	================================================== -->
-	<meta charset="utf-8">
-	<meta name="description" content="">
-	<meta name="author" content="">
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<!-- mobile specific metas
+    <!-- mobile specific metas
 	================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 	<?php wp_head(); ?>
 
@@ -22,166 +22,141 @@
 ================================================== -->
 <section class="s-pageheader s-pageheader--home">
 
-	<header class="header">
-		<div class="header__content row">
+    <header class="header">
+        <div class="header__content row">
 
-			<div class="header__logo">
-				<a class="logo" href="index.html">
-					<img src="<?php echo get_template_directory_uri() ?>/assets/images/logo.svg" alt="Homepage">
-				</a>
-			</div> <!-- end header__logo -->
+            <div class="header__logo">
+                <a class="logo" href="index.html">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/logo.svg" alt="Homepage">
+                </a>
+            </div> <!-- end header__logo -->
 
-			<ul class="header__social">
-				<li>
-					<a href="#0"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-				</li>
-				<li>
-					<a href="#0"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-				</li>
-				<li>
-					<a href="#0"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-				</li>
-				<li>
-					<a href="#0"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-				</li>
-			</ul> <!-- end header__social -->
+            <ul class="header__social">
+                <li>
+                    <a href="#0"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                    <a href="#0"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                    <a href="#0"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                    <a href="#0"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                </li>
+            </ul> <!-- end header__social -->
 
-			<a class="header__search-trigger" href="#0"></a>
+            <a class="header__search-trigger" href="#0"></a>
 
-			<div class="header__search">
+            <div class="header__search">
 
-				<form role="search" method="get" class="header__search-form" action="#">
-					<label>
-						<span class="hide-content">Search for:</span>
-						<input type="search" class="search-field" placeholder="Type Keywords" value="" name="s"
-							   title="Search for:" autocomplete="off">
-					</label>
-					<input type="submit" class="search-submit" value="Search">
-				</form>
+                <form role="search" method="get" class="header__search-form" action="#">
+                    <label>
+                        <span class="hide-content">Search for:</span>
+                        <input type="search" class="search-field" placeholder="Type Keywords" value="" name="s"
+                               title="Search for:" autocomplete="off">
+                    </label>
+                    <input type="submit" class="search-submit" value="Search">
+                </form>
 
-				<a href="#0" title="Close Search" class="header__overlay-close">Close</a>
+                <a href="#0" title="Close Search" class="header__overlay-close">Close</a>
 
-			</div>  <!-- end header__search -->
-
-
-			<a class="header__toggle-menu" href="#0" title="Menu"><span>Menu</span></a>
-
-			<nav class="header__nav-wrap">
-
-				<h2 class="header__nav-heading h6">Site Navigation</h2>
-
-				<ul class="header__nav">
-					<li class="current"><a href="index.html" title="">Home</a></li>
-					<li class="has-children">
-						<a href="#0" title="">Categories</a>
-						<ul class="sub-menu">
-							<li><a href="category.html">Lifestyle</a></li>
-							<li><a href="category.html">Health</a></li>
-							<li><a href="category.html">Family</a></li>
-							<li><a href="category.html">Management</a></li>
-							<li><a href="category.html">Travel</a></li>
-							<li><a href="category.html">Work</a></li>
-						</ul>
-					</li>
-					<li class="has-children">
-						<a href="#0" title="">Blog</a>
-						<ul class="sub-menu">
-							<li><a href="single-video.html">Video Post</a></li>
-							<li><a href="single-audio.html">Audio Post</a></li>
-							<li><a href="single-gallery.html">Gallery Post</a></li>
-							<li><a href="single-standard.html">Standard Post</a></li>
-						</ul>
-					</li>
-					<li><a href="style-guide.html" title="">Styles</a></li>
-					<li><a href="about.html" title="">About</a></li>
-					<li><a href="contact.html" title="">Contact</a></li>
-				</ul> <!-- end header__nav -->
-
-				<a href="#0" title="Close Menu" class="header__overlay-close close-mobile-menu">Close</a>
-
-			</nav> <!-- end header__nav-wrap -->
-
-		</div> <!-- header-content -->
-	</header> <!-- header -->
+            </div>  <!-- end header__search -->
 
 
-	<div class="pageheader-content row">
-		<div class="col-full">
 
-			<div class="featured">
+			<?php get_template_part("template-parts/common/navigation"); ?>
 
-				<div class="featured__column featured__column--big">
-					<div class="entry" style="background-image:url('<?php echo get_template_directory_uri() ?>/assets/images/thumbs/featured/featured-guitarman.jpg');">
+        </div> <!-- header-content -->
+    </header> <!-- header -->
 
-						<div class="entry__content">
-							<span class="entry__category"><a href="#0">Music</a></span>
 
-							<h1><a href="#0" title="">What Your Music Preference Says About You and Your
-									Personality.</a></h1>
+    <div class="pageheader-content row">
+        <div class="col-full">
 
-							<div class="entry__info">
-								<a href="#0" class="entry__profile-pic">
-									<img class="avatar" src="<?php echo get_template_directory_uri() ?>/assets/images/avatars/user-03.jpg" alt="">
-								</a>
+            <div class="featured">
 
-								<ul class="entry__meta">
-									<li><a href="#0">John Doe</a></li>
-									<li>December 29, 2017</li>
-								</ul>
-							</div>
-						</div> <!-- end entry__content -->
+                <div class="featured__column featured__column--big">
+                    <div class="entry"
+                         style="background-image:url('<?php echo get_template_directory_uri() ?>/assets/images/thumbs/featured/featured-guitarman.jpg');">
 
-					</div> <!-- end entry -->
-				</div> <!-- end featured__big -->
+                        <div class="entry__content">
+                            <span class="entry__category"><a href="#0">Music</a></span>
 
-				<div class="featured__column featured__column--small">
+                            <h1><a href="#0" title="">What Your Music Preference Says About You and Your
+                                    Personality.</a></h1>
 
-					<div class="entry" style="background-image:url('<?php echo get_template_directory_uri() ?>/assets/images/thumbs/featured/featured-watch.jpg');">
+                            <div class="entry__info">
+                                <a href="#0" class="entry__profile-pic">
+                                    <img class="avatar"
+                                         src="<?php echo get_template_directory_uri() ?>/assets/images/avatars/user-03.jpg"
+                                         alt="">
+                                </a>
 
-						<div class="entry__content">
-							<span class="entry__category"><a href="#0">Management</a></span>
+                                <ul class="entry__meta">
+                                    <li><a href="#0">John Doe</a></li>
+                                    <li>December 29, 2017</li>
+                                </ul>
+                            </div>
+                        </div> <!-- end entry__content -->
 
-							<h1><a href="#0" title="">The Pomodoro Technique Really Works.</a></h1>
+                    </div> <!-- end entry -->
+                </div> <!-- end featured__big -->
 
-							<div class="entry__info">
-								<a href="#0" class="entry__profile-pic">
-									<img class="avatar" src="<?php echo get_template_directory_uri() ?>/assets/images/avatars/user-03.jpg" alt="">
-								</a>
+                <div class="featured__column featured__column--small">
 
-								<ul class="entry__meta">
-									<li><a href="#0">John Doe</a></li>
-									<li>December 27, 2017</li>
-								</ul>
-							</div>
-						</div> <!-- end entry__content -->
+                    <div class="entry"
+                         style="background-image:url('<?php echo get_template_directory_uri() ?>/assets/images/thumbs/featured/featured-watch.jpg');">
 
-					</div> <!-- end entry -->
+                        <div class="entry__content">
+                            <span class="entry__category"><a href="#0">Management</a></span>
 
-					<div class="entry" style="background-image:url('<?php echo get_template_directory_uri() ?>/assets/images/thumbs/featured/featured-beetle.jpg');">
+                            <h1><a href="#0" title="">The Pomodoro Technique Really Works.</a></h1>
 
-						<div class="entry__content">
-							<span class="entry__category"><a href="#0">LifeStyle</a></span>
+                            <div class="entry__info">
+                                <a href="#0" class="entry__profile-pic">
+                                    <img class="avatar"
+                                         src="<?php echo get_template_directory_uri() ?>/assets/images/avatars/user-03.jpg"
+                                         alt="">
+                                </a>
 
-							<h1><a href="#0" title="">Throwback To The Good Old Days.</a></h1>
+                                <ul class="entry__meta">
+                                    <li><a href="#0">John Doe</a></li>
+                                    <li>December 27, 2017</li>
+                                </ul>
+                            </div>
+                        </div> <!-- end entry__content -->
 
-							<div class="entry__info">
-								<a href="#0" class="entry__profile-pic">
-									<img class="avatar" src="<?php echo get_template_directory_uri() ?>/assets/images/avatars/user-03.jpg" alt="">
-								</a>
+                    </div> <!-- end entry -->
 
-								<ul class="entry__meta">
-									<li><a href="#0">John Doe</a></li>
-									<li>December 21, 2017</li>
-								</ul>
-							</div>
-						</div> <!-- end entry__content -->
+                    <div class="entry"
+                         style="background-image:url('<?php echo get_template_directory_uri() ?>/assets/images/thumbs/featured/featured-beetle.jpg');">
 
-					</div> <!-- end entry -->
+                        <div class="entry__content">
+                            <span class="entry__category"><a href="#0">LifeStyle</a></span>
 
-				</div> <!-- end featured__small -->
-			</div> <!-- end featured -->
+                            <h1><a href="#0" title="">Throwback To The Good Old Days.</a></h1>
 
-		</div> <!-- end col-full -->
-	</div> <!-- end pageheader-content row -->
+                            <div class="entry__info">
+                                <a href="#0" class="entry__profile-pic">
+                                    <img class="avatar"
+                                         src="<?php echo get_template_directory_uri() ?>/assets/images/avatars/user-03.jpg"
+                                         alt="">
+                                </a>
+
+                                <ul class="entry__meta">
+                                    <li><a href="#0">John Doe</a></li>
+                                    <li>December 21, 2017</li>
+                                </ul>
+                            </div>
+                        </div> <!-- end entry__content -->
+
+                    </div> <!-- end entry -->
+
+                </div> <!-- end featured__small -->
+            </div> <!-- end featured -->
+
+        </div> <!-- end col-full -->
+    </div> <!-- end pageheader-content row -->
 
 </section> <!-- end s-pageheader -->
