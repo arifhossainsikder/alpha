3b0fa66ae7c081a6db576e45c7aea87136bec636
Arif <arifcse11@gmail.com>
2019-01-09 00:25:28 +0600

    gallery post and audio video post done

--- wp-content/themes/philosophy/inc/tgm.php
+++ wp-content/themes/philosophy/inc/tgm.php
@@ -31,9 +31,9 @@
  * Plugin:
  * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
  */
-require_once get_theme_file_path( '/lib/class-tgm-plugin-activation.php' );
+require_once get_theme_file_path('/lib/class-tgm-plugin-activation.php');
 
-add_action( 'tgmpa_register', 'philosophy_register_required_plugins' );
+add_action('tgmpa_register', 'philosophy_register_required_plugins');
 
 /**
  * Register the required plugins for this theme.
@@ -52,53 +52,59 @@ add_action( 'tgmpa_register', 'philosophy_register_required_plugins' );
  *
  * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
-function philosophy_register_required_plugins() {
-    /*
-     * Array of plugin arrays. Required keys are name and slug.
-     * If the source is NOT from the .org repo, then source is also required.
-     */
-    $plugins = array(
+function philosophy_register_required_plugins()
+{
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
 
 
-        // This is an example of how to include a plugin from the WordPress Plugin Repository.
-        array(
-            'name'     => 'Advanced Custom Fields',
-            'slug'     => 'advanced-custom-fields',
-            'required' => false,
-        ),
+		// This is an example of how to include a plugin from the WordPress Plugin Repository.
+		array(
+			'name' => 'Advanced Custom Fields',
+			'slug' => 'advanced-custom-fields',
+			'required' => false,
+		),
+		array(
+			'name' => 'Attachments',
+			'slug' => 'attachments',
+			'required' => false,
+		),
 
 
-    );
+	);
 
-    /*
-     * Array of configuration settings. Amend each line as needed.
-     *
-     * TGMPA will start providing localized text strings soon. If you already have translations of our standard
-     * strings available, please help us make TGMPA even better by giving us access to these translations or by
-     * sending in a pull-request with .po file(s) with the translations.
-     *
-     * Only uncomment the strings in the config array if you want to customize the strings.
-     */
-    $config = array(
-        'id'           => 'philosophy',
-        // Unique ID for hashing notices for multiple instances of TGMPA.
-        'default_path' => '',
-        // Default absolute path to bundled plugins.
-        'menu'         => 'tgmpa-install-plugins',
-        // Menu slug.
-        'has_notices'  => true,
-        // Show admin notices or not.
-        'dismissable'  => true,
-        // If false, a user cannot dismiss the nag message.
-        'dismiss_msg'  => '',
-        // If 'dismissable' is false, this message will be output at top of nag.
-        'is_automatic' => false,
-        // Automatically activate plugins after installation or not.
-        'message'      => '',
-        // Message to output right before the plugins table.
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 *
+	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
+	 * sending in a pull-request with .po file(s) with the translations.
+	 *
+	 * Only uncomment the strings in the config array if you want to customize the strings.
+	 */
+	$config = array(
+		'id' => 'philosophy',
+		// Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',
+		// Default absolute path to bundled plugins.
+		'menu' => 'tgmpa-install-plugins',
+		// Menu slug.
+		'has_notices' => true,
+		// Show admin notices or not.
+		'dismissable' => true,
+		// If false, a user cannot dismiss the nag message.
+		'dismiss_msg' => '',
+		// If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,
+		// Automatically activate plugins after installation or not.
+		'message' => '',
+		// Message to output right before the plugins table.
 
 
-    );
+	);
 
-    tgmpa( $plugins, $config );
+	tgmpa($plugins, $config);
 }
